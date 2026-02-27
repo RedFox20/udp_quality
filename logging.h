c2f2d8071c098accf9a0aadedf81830844c69242
Jorma Rebane <jorma.rebane@gmail.com>
2026-02-27 20:35:18 +0200

    fix: remove duplicate color defines

--- logging.h
+++ logging.h
@@ -2,14 +2,6 @@
 #include <rpp/sockets.h>
 #include <rpp/debugging.h>
 
-#define RED(text)     "\x1b[91m" text "\x1b[0m"
-#define GREEN(text)   "\x1b[92m" text "\x1b[0m"
-#define ORANGE(text)  "\x1b[93m" text "\x1b[0m"
-#define BLUE(text)    "\x1b[94m" text "\x1b[0m"
-#define MAGENTA(text) "\x1b[95m" text "\x1b[0m"
-#define CYAN(text)    "\x1b[96m" text "\x1b[0m"
-#define WHITE(text)   "\x1b[97m" text "\x1b[0m"
-
 // prints socket error and exits
 #define LogErrorExit(format, ...) do { \
     std::string _socket_err = rpp::socket::last_os_socket_err(); \
